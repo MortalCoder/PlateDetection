a129bdc45a2420e6300bf5f1ec9e9a3c1e6b8aeb
Vladimir <vvbedash@gmail.com>
2024-07-15 10:36:31 +0500

    Camera test

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import streamlit as st
+import time
 
-"""
-# Bruh
-"""
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+
+
+if st.button('Калькулировать', type="primary"):
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
+
+
+st.camera_input("Bruh")
